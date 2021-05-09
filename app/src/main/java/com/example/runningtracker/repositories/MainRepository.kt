b37e36ac858c8401fab6b0f45f7a93d22b6d44ef
HaRsH8747 <66661386+HaRsH8747@users.noreply.github.com>
2021-05-09 16:18:16 +0530

    Sixth commit

---
+++ app/src/main/java/com/example/runningtracker/repositories/MainRepository.kt
@@ -0,0 +1,35 @@
+package com.example.runningtracker.repositories
+
+import com.example.runningtracker.db.Run
+import com.example.runningtracker.db.RunDAO
+import javax.inject.Inject
+
+
+class MainRepository @Inject constructor(
+    val runDAO: RunDAO
+) {
+
+    suspend fun insertRun(run: Run) = runDAO.insertRun(run)
+
+    suspend fun deleteRun(run: Run) = runDAO.deleteRun(run)
+
+
+    fun getAllRunsSortedByDate() = runDAO.getAllRunsSortedByDate()
+
+    fun getAllRunsSortedByAvgSpeed() = runDAO.getAllRunsSortedByAvgSpeed()
+
+    fun getAllRunsSortedByDistance() = runDAO.getAllRunsSortedByDistance()
+
+    fun getAllRunsSortedByTimeInMillis() = runDAO.getAllRunsSortedByTimeInMillis()
+
+    fun getAllRunsSortedByCaloriesBurned() = runDAO.getAllRunsSortedByCaloriesBurned()
+
+
+    fun getTotalAvgSpeed() = runDAO.getTotalAvgSpeed()
+
+    fun getTotalDistance() = runDAO.getTotalDistance()
+
+    fun getTotalTimeInMillis() = runDAO.getTotalTimeInMillis()
+
+    fun getTotalCaloriesBurned() = runDAO.getTotalCaloriesBurned()
+}
---
+++ app/src/main/java/com/example/runningtracker/ui/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.runningtracker.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.runningtracker.R
+import com.example.runningtracker.db.RunDAO
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment = this.findNavController(R.id.navHostFragment)
+        bottomNavigationView.setupWithNavController(navHostFragment)
+
+        navHostFragment.addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id){
+                R.id.runFragment, R.id.statisticsFragment, R.id.settingsFragment ->
+                    bottomNavigationView.visibility = View.VISIBLE
+                else -> bottomNavigationView.visibility = View.GONE
+            }
+        }
+    }
+}
